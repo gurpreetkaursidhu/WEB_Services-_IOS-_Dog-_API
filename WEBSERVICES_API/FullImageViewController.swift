3daca807878bc084ea3fb5999568a1c7367e045e
gurpreetkaur <A00252915@mycambrian.ca>
2022-10-29 14:18:00 -0400

    Via segue operation Full image view controller able to fetch  the  more images of the selected breed

--- WEBSERVICES_API/FullImageViewController.swift
+++ WEBSERVICES_API/FullImageViewController.swift
@@ -8,83 +8,61 @@
 import UIKit
 
 class FullImageViewController: UITableViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
         var dogName: String!
         let URL: String = "https://dog.ceo/api/breed/"
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+      
+        @IBOutlet weak var FullImage: UIImageView!
+        @IBOutlet weak var dogBreed: UILabel!
+        @IBOutlet weak var spinner: UIActivityIndicatorView!
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+        
+            spinner.startAnimating()
+            
+            dogBreed.text = dogName
+            let url: String = URL + dogName + "/images"
+            //fetch dog image by dog breed name
+            APIHelper.fetchImage(url: url ) {dogImage in
+                //pause
+                for _ in 0...1000000{
+                    continue
+                }
+                do{
+                    try
+                    //set image the first json image url and transfer it to UIImage
+                    self.FullImage.image = UIImage(data: NSData(contentsOf: NSURL(string: dogImage[0])! as URL) as Data)
+                    self.spinner.isHidden = true
+                }catch let error{
+                    print(error)
+                }
+            }
+        }
+        @IBAction func NewDogImageButton(_ sender: Any) {
+            let url: String = URL + dogName + "/images"
+            //fetch dog image by dog breed name
+            APIHelper.fetchImage(url: url ) {dogImage in
+                //pause
+                for _ in 0...1000000{
+                    continue
+                }
+                
+                do{
+                    try
+                    //set image in the random json image url and transfer it to UIImage
+                    self.FullImage.image = UIImage(data: NSData(contentsOf: NSURL(string: dogImage.randomElement()!)! as URL) as Data)
+                    
+                }catch let error{
+                    print(error)
+                }
+            }
+        }
+        
+        
     }
+    
+    
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
